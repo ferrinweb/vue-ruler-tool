4086398e77976fa9bb107c8539bfd119d869e00b
ferrinweb <ferrinweb@gmail.com>
2020-03-25 19:15:21 +0800

    修复参考线负值bug；增加容器拖动、标尺参考线随动功能；标尺刻度跟随容器尺度生成；

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <button @click="visible = !visible">显示/隐藏标尺</button>
-    <button @click="presetLine = []">清空</button>
-    <button @click="presetLine = [{ type: 'h', site: 100 }, { type: 'v', site: 200 }]">修改</button>
-    <section style="margin: 50px;padding: 50px;border: 1px solid red;height: 600px;">
+    <div class="action-bar">
+      <button @click="visible = !visible">显示/隐藏标尺</button>
+      <button @click="presetLine = []">清空</button>
+      <button @click="presetLine = [{ type: 'h', site: 100 }, { type: 'v', site: 200 }]">修改</button>
+    </div>
+    <section style="height: 100%; width: 100%;">
       <vue-ruler-tool
         v-model="presetLine"
-        :step-length="20"
+        :step-length="50"
         :parent="true"
         :is-scale-revise="true"
         :visible.sync="visible"
+        :contentLayout="{top: 100, left: 100}"
       >
-        <img src="https://cn.vuejs.org/images/logo.png" style="width: 300px;height: 300px;margin: 100px;" alt="">
+        <img src="https://cn.vuejs.org/images/logo.png" style="width: 3000px;height: 3000px;margin: 100px;" alt="">
          <!--<iframe src="https://www.baidu.com" width="500" height="300" />-->
       </vue-ruler-tool>
     </section>
@@ -40,9 +43,22 @@ export default {
 </script>
 
 <style lang="scss">
+html, body{
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+}
+.action-bar{
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  z-index: 4;
 }
 </style>
--- src/components/vue-ruler-tool.vue
+++ src/components/vue-ruler-tool.vue
@@ -1,14 +1,28 @@
 <template>
   <div :style="wrapperStyle" class="vue-ruler-wrapper" onselectstart="return false;">
     <section v-show="rulerToggle">
-      <div ref="horizontalRuler" class="vue-ruler-h" @mousedown.stop="horizontalDragRuler">
-        <span v-for="(item,index) in xScale" :key="index" :style="{left:index * 50 + 2 + 'px'}" class="n">{{ item.id }}</span>
+      <div class="ruler-wrapper h">
+        <div
+          ref="horizontalRuler"
+          class="vue-ruler-h"
+          :style="`width: ${hrWidth + contentLayout.left * 2}px; transform: translateX(${contentScrollLeft}px)`"
+          @mousedown.stop="horizontalDragRuler"
+        >
+          <span v-for="(item,index) in xScale" :key="index" :style="{left:index * 50 + 2 + 'px'}" class="n">{{ item.id }}</span>
+        </div>
       </div>
-      <div ref="verticalRuler" class="vue-ruler-v" @mousedown.stop="verticalDragRuler">
-        <span v-for="(item,index) in yScale" :key="index" :style="{top:index * 50 + 2 + 'px'}" class="n">{{ item.id }}</span>
+      <div class="ruler-wrapper v">
+        <div
+          ref="verticalRuler"
+          class="vue-ruler-v"
+          :style="`height: ${vrHeight + contentLayout.top * 2}px; transform: translateY(${contentScrollTop}px)`"
+          @mousedown.stop="verticalDragRuler"
+        >
+          <span v-for="(item,index) in yScale" :key="index" :style="{top:index * 50 + 2 + 'px'}" class="n">{{ item.id }}</span>
+        </div>
       </div>
-      <div :style="{top:verticalDottedTop + 'px'}" class="vue-ruler-ref-dot-h" />
-      <div :style="{left:horizontalDottedLeft + 'px'}" class="vue-ruler-ref-dot-v" />
+      <div :style="{transform:`translateY(${verticalDottedTop}px)`}" class="vue-ruler-ref-dot-h" />
+      <div :style="{transform:`translateX(${horizontalDottedLeft}px)`}" class="vue-ruler-ref-dot-v" />
       <div
         v-for="item in lineList"
         :title="item.title"
@@ -17,7 +31,14 @@
         :class="`vue-ruler-ref-line-${item.type}`"
         @mousedown="handleDragLine(item)"></div>
     </section>
-    <div ref="content" class="vue-ruler-content" :style="contentStyle">
+    <div
+      ref="content"
+      class="vue-ruler-content"
+      :style="contentStyle"
+      @mousedown.stop="markContentMoveStart($event)"
+      @mouseup.stop="markContentMoveEnd($event)"
+      @mousemove.prevent
+    >
       <slot />
     </div>
     <div v-show="isDrag" class="vue-ruler-content-mask"></div>
@@ -80,6 +101,7 @@ export default {
       topSpacing: 0, // 标尺与窗口上间距
       leftSpacing: 0, //  标尺与窗口左间距
       isDrag: false,
+      isMoved: false,
       dragFlag: '', // 拖动开始标记，可能值x(从水平标尺开始拖动),y(从垂直标尺开始拖动)
       horizontalDottedLeft: -999, // 水平虚线位置
       verticalDottedTop: -999, // 垂直虚线位置
@@ -89,7 +111,15 @@ export default {
       keyCode: {
         r: 82
       }, // 快捷键参数
-      rulerToggle: true // 标尺辅助线显示开关
+      rulerToggle: true, // 标尺辅助线显示开关
+      hrWidth: 0,
+      vrHeight: 0,
+      contentMoveStartX: 0,
+      contentMoveStartY: 0,
+      contentScrollLeft: 0,
+      contentScrollTop: 0,
+      contentMove: false,
+      contentDrag: false
     }
   },
   computed: {
@@ -100,11 +130,26 @@ export default {
         position: this.position
       }
     },
+    contentScrollRangeX () {
+      return this.contentRef.scrollWidth - this.windowWidth
+    },
+    contentScrollRangeY () {
+      return this.contentRef.scrollHeight - this.windowHeight
+    },
+    contentRef () {
+      return this.$refs.content || {}
+    },
+    contentRect () {
+      return this.$refs.content && this.$refs.content.getBoundingClientRect() || {}
+    },
     contentStyle() {
       return {
         left: this.contentLayout.left + 'px',
         top: this.contentLayout.top + 'px',
-        padding: this.left_top + 'px 0px 0px ' + this.left_top + 'px'
+        padding: this.left_top + 'px 0px 0px ' + this.left_top + 'px',
+        cursor: this.contentMove ? 'move' : '',
+        transform: `translate3d(${this.contentScrollLeft}px, ${this.contentScrollTop}px, 0)`
+        // transition: this.contentDrag ? 'transform 0s' : ''
       }
     },
     lineList() {
@@ -116,9 +161,10 @@ export default {
           id: `${item.type}_${isH ? hCount++ : vCount++}`,
           type: item.type,
           title: item.site.toFixed(2) + 'px',
+          site: item.site,
           [isH ? 'top' : 'left']: item.site / (this.stepLength / 50) + this.size
         }
-      })
+      }).filter(item => item.site > -18)
     }
   },
   watch: {
@@ -133,40 +179,65 @@ export default {
     on(document, 'mousemove', this.dottedLineMove)
     on(document, 'mouseup', this.dottedLineUp)
     on(document, 'keyup', this.keyboard)
-    this.init()
+    on(document, 'keydown', this.toggleContentMove)
     on(window, 'resize', this.windowResize)
+    on(window, 'scroll', this.setSpacing)
+    this.init()
   },
   beforeDestroy () {
     off(document, 'mousemove', this.dottedLineMove)
     off(document, 'mouseup', this.dottedLineUp)
     off(document, 'keyup', this.keyboard)
+    off(document, 'keydown', this.toggleContentMove)
     off(window, 'resize', this.windowResize)
+    off(window, 'scroll', this.setSpacing)
   },
   methods: {
     init () {
       this.box()
-      this.scaleCalc()
+      this.setSpacing()
+      this.updateRulerRange()
     },
     windowResize() {
       this.xScale = []
       this.yScale = []
       this.init()
     },
-    getLineStyle({type, top, left}) {
-      return type === 'h' ? {top: top + 'px'} : {left: left + 'px'}
+    updateRulerRange () {
+      const width = this.contentRef.scrollWidth
+      this.hrWidth = width && width > this.windowWidth ? width : this.windowWidth
+      const height = this.contentRef.scrollHeight
+      this.vrHeight =  height && height > this.windowHeight ? height : this.windowHeight
+      this.scaleCalc()
+    },
+    updateContentPos () {
+      this.contentScrollLeft = this.contentRef.scrollLeft
+      this.contentScrollTop = this.contentRef.scrollTop
+    },
+    toggleContentMove (e) {
+      if (e.keyCode === 17) this.contentMove = true
+    },
+    markContentMoveStart (e) {
+      this.contentMoveStartX = e.clientX
+      this.contentMoveStartY = e.clientY
+      this.contentDrag = true
+    },
+    markContentMoveEnd () {
+      this.contentMoveStartX = this.contentMoveStartY = 0
+      this.contentDrag = false
+    },
+    getLineStyle({type, top, left, site}) {
+      const style = [type === 'h' ? `top: ${top}px` : `left: ${left}px`]
+      site < 0 && style.push('opacity: 0')
+      this.contentMove && style.push('pointer-events: none')
+      type === 'h' && style.push(`transform: translateY(${this.contentScrollTop}px)`)
+      type === 'v' && style.push(`transform: translateX(${this.contentScrollLeft}px)`)
+      return style.join(';')
     },
     handleDragLine({type, id}) {
       return type === 'h' ? this.dragHorizontalLine(id) : this.dragVerticalLine(id)
     },
     box () {
-      if (this.isScaleRevise) { // 根据内容部分进行刻度修正
-        const content = this.$refs.content
-        const contentLeft = content.offsetLeft
-        const contentTop = content.offsetTop
-
-        this.getCalcRevise(this.xScale, contentLeft)
-        this.getCalcRevise(this.yScale, contentTop)
-      }
       if (this.parent) {
         const style = window.getComputedStyle(this.$el.parentNode, null)
         this.windowWidth = parseInt(style.getPropertyValue('width'), 10)
@@ -177,15 +248,20 @@ export default {
       }
       this.rulerWidth = this.$refs.verticalRuler.clientWidth
       this.rulerHeight = this.$refs.horizontalRuler.clientHeight
-      this.setSpacing()
     }, // 获取窗口宽与高
     setSpacing () {
       this.topSpacing = this.$refs.horizontalRuler.getBoundingClientRect().y //.offsetParent.offsetTop
       this.leftSpacing = this.$refs.verticalRuler.getBoundingClientRect().x// .offsetParent.offsetLeft
     },
     scaleCalc () {
-      this.getCalc(this.xScale, this.windowWidth)
-      this.getCalc(this.yScale, this.windowHeight)
+      const content = this.contentLayout
+      // 修正刻度
+      const contentLeft = this.isScaleRevise ? content.left : 0
+      const contentTop = this.isScaleRevise ? content.top : 0
+      this.getCalcRevise(this.xScale, contentLeft)
+      this.getCalcRevise(this.yScale, contentTop)
+      this.getCalc(this.xScale, this.hrWidth + contentLeft)
+      this.getCalc(this.yScale, this.vrHeight + contentTop)
     }, // 计算刻度
     getCalc (array,length) {
       for (let i = 0; i < length * this.stepLength / 50; i += this.stepLength) {
@@ -195,7 +271,8 @@ export default {
       }
     }, // 获取刻度方法
     getCalcRevise (array,length) {
-      for (let i = 0; i < length; i += 1) {
+      array.splice(0)
+      for (let i = -length * this.stepLength / 50; i < 0; i += this.stepLength) {
         if (i % this.stepLength === 0 && i + this.stepLength <= length) {
           array.push({ id: i })
         }
@@ -206,65 +283,97 @@ export default {
       this.dragFlag = val
     }, // 生成一个参考线
     dottedLineMove ($event) {
-      this.setSpacing()
+      const { clientX, clientY } = $event
+      if (this.contentMove && this.contentDrag) {
+        if (!this.contentMoveStartX && !this.contentMoveStartX) {
+          this.contentMoveStartX = clientX
+          this.contentMoveStartY = clientY
+        }
+        this.contentScrollLeft += clientX - this.contentMoveStartX
+        this.contentScrollTop += clientY - this.contentMoveStartY
+        if (this.contentScrollLeft > 0) this.contentScrollLeft = 0
+        if (this.contentScrollTop > 0) this.contentScrollTop = 0
+        const endX = -this.contentScrollRangeX - this.contentLayout.left * 2
+        const endY = -this.contentScrollRangeY - this.contentLayout.top * 2
+        if (this.contentScrollLeft < endX) this.contentScrollLeft = endX
+        if (this.contentScrollTop < endY) this.contentScrollTop = endY
+        this.contentMoveStartX = clientX
+        this.contentMoveStartY = clientY
+        return
+      }
+      if (!this.isDrag) return
+      this.isMoved = true
       switch (this.dragFlag) {
         case 'x':
-          if (this.isDrag) {
-            this.verticalDottedTop = $event.pageY - this.topSpacing
-          }
+          this.verticalDottedTop = clientY - this.topSpacing
           break
         case 'y':
-          if (this.isDrag) {
-            this.horizontalDottedLeft = $event.pageX - this.leftSpacing
-          }
+          this.horizontalDottedLeft = clientX - this.leftSpacing
           break
         case 'h':
-          if (this.isDrag) {
-            this.verticalDottedTop = $event.pageY - this.topSpacing
-          }
+          this.verticalDottedTop = clientY - this.topSpacing
           break
         case 'v':
-          if (this.isDrag) {
-            this.horizontalDottedLeft = $event.pageX - this.leftSpacing
-          }
+          this.horizontalDottedLeft = clientX - this.leftSpacing
           break
         default:
           break
       }
     }, // 虚线移动
     dottedLineUp ($event) {
-      this.setSpacing()
-      if (this.isDrag) {
-        this.isDrag = false
-        const cloneList = JSON.parse(JSON.stringify(this.value))
+      if (!this.isDrag)  return
+      const { clientX, clientY } = $event
+      if (!this.isMoved) {
+        if (this.verticalDottedTop !== -999 || this.horizontalDottedLeft !== -999) {
+          this.verticalDottedTop = this.horizontalDottedLeft = -999
+          this.isDrag = false
+          return
+        }
         switch (this.dragFlag) {
           case 'x':
-            cloneList.push({
-              type: 'h',
-              site: ($event.pageY - this.topSpacing - this.size) * (this.stepLength / 50)
-            })
-            this.$emit('input', cloneList)
+            this.verticalDottedTop = clientY - this.topSpacing
             break
           case 'y':
-            cloneList.push({
-              type: 'v',
-              site: ($event.pageX - this.leftSpacing - this.size) * (this.stepLength / 50)
-            })
-            this.$emit('input', cloneList)
+            this.horizontalDottedLeft = clientX - this.leftSpacing
             break
           case 'h':
-            this.dragCalc(cloneList, $event.pageY, this.topSpacing, this.rulerHeight,'h')
-            this.$emit('input', cloneList)
+            this.verticalDottedTop = clientY - this.topSpacing
             break
           case 'v':
-            this.dragCalc(cloneList, $event.pageX, this.leftSpacing, this.rulerWidth,'v')
-            this.$emit('input', cloneList)
+            this.horizontalDottedLeft = clientX - this.leftSpacing
             break
           default:
             break
         }
-        this.verticalDottedTop = this.horizontalDottedLeft = -10
+        return
       }
+      this.isDrag = false
+      this.isMoved = false
+      const cloneList = JSON.parse(JSON.stringify(this.lineList))
+      switch (this.dragFlag) {
+        case 'x':
+          cloneList.push({
+            type: 'h',
+            site: (clientY - this.topSpacing - this.size) * (this.stepLength / 50) - this.contentScrollTop | 0
+          })
+          break
+        case 'y':
+          cloneList.push({
+            type: 'v',
+            site: (clientX - this.leftSpacing - this.size) * (this.stepLength / 50) - this.contentScrollLeft | 0
+          })
+          break
+        case 'h':
+          this.dragCalc(cloneList, clientY, this.topSpacing, this.rulerHeight,'h')
+          break
+        case 'v':
+          this.dragCalc(cloneList, clientX, this.leftSpacing, this.rulerWidth,'v')
+          break
+        default:
+          break
+      }
+      this.$emit('input', cloneList)
+      this.verticalDottedTop = this.horizontalDottedLeft = -999
     }, // 虚线松开
     dragCalc (list, page, spacing, ruler, type) {
       if (page - spacing < ruler) {
@@ -275,10 +384,7 @@ export default {
             id = item.id
           }
         })
-        list.splice(Index, 1, {
-          type: type,
-          site: -600
-        })
+        list.splice(Index, 1)
       } else {
         let Index, id
         this.lineList.forEach((item, index) => {
@@ -321,6 +427,10 @@ export default {
               this.left_top = 0;
             }
             break
+          case 17:
+            this.contentMove = false
+            this.contentMoveStartX = this.contentMoveStartY = 0
+            break
         }
       }
     }, // 键盘事件
@@ -329,11 +439,36 @@ export default {
 </script>
 
 <style lang="scss">
+.ruler-wrapper {
+  position: absolute;
+  box-shadow:#111 0 0 1px;
+  &.h{
+    width: calc(100% - 18px);
+    height: 18px;
+    top: 0;
+    left: 18px;
+    &:before{
+      content:" ";
+      position: absolute;
+      left: -18px;
+      width: 18px;
+      height: 18px;
+      z-index: 1000;
+      background-color: #111;
+    }
+  }
+  &.v{
+    width: 18px;
+    height: calc(100% - 18px);
+    left: 0;
+    top: 18px;
+  }
+}
 .vue-ruler{
   &-wrapper {
     left: 0;
     top: 0;
-    z-index: 999;
+    z-index: 1;
     overflow: hidden;
     user-select: none;
   }
@@ -347,43 +482,38 @@ export default {
     left: 0;
     top: 0;
     overflow: hidden;
-    z-index: 999;
-  }
-  &-h,
-  &-v,
-  &-ref-line-v,
-  &-ref-line-h,
-  &-ref-dot-h,
-  &-ref-dot-v {
-    position: absolute;
-    left: 0;
-    top: 0;
-    overflow: hidden;
-    z-index: 999;
+    z-index: 3;
   }
 
+  &-ref-line-v,
+  &-ref-line-h,
+  &-h,
+  &-v,
+  &-content {
+    transition: transform .4s;
+  }
   &-h {
-    width: 100%;
     height: 18px;
-    left: 18px;
+    left: 0;
     opacity: 0.6;
     background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAASCAMAAAAuTX21AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAlQTFRFMzMzAAAA////BqjYlAAAACNJREFUeNpiYCAdMDKRCka1jGoBA2JZZGshiaCXFpIBQIABAAplBkCmQpujAAAAAElFTkSuQmCC)
-      repeat-x; /*./image/ruler_h.png*/
+      repeat-x;
+    cursor: n-resize;
   }
 
   &-v {
     width: 18px;
-    height: 100%;
-    top: 18px;
+    top: 0;
     opacity: 0.6;
     background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAyCAMAAABmvHtTAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAlQTFRFMzMzAAAA////BqjYlAAAACBJREFUeNpiYGBEBwwMTGiAakI0NX7U9aOuHyGuBwgwAH6bBkAR6jkzAAAAAElFTkSuQmCC)
-      repeat-y; /*./image/ruler_v.png*/
+      repeat-y;
+    cursor: w-resize;
   }
 
   &-v .n,
   &-h .n {
     position: absolute;
-    font: 10px/1 Arial, sans-serif;
+    font: 10px/1 serif;
     color: #333;
     cursor: default;
   }
@@ -396,13 +526,6 @@ export default {
 
   &-h .n {
     top: 1px;
-  }
-
-  &-ref-line-v,
-  &-ref-line-h,
-  &-ref-dot-h,
-  &-ref-dot-v {
-    z-index: 998;
   }
 
   &-ref-line-h {
@@ -428,7 +551,7 @@ export default {
     background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAMAAABFaP0WAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAZQTFRFf39/////F3PnHQAAAAJ0Uk5T/wDltzBKAAAAEElEQVR42mJgYGRgZAQIMAAADQAExkizYQAAAABJRU5ErkJggg==)
       repeat-x left 1px; /*./image/line_dot.png*/
     cursor: n-resize; /*url(./image/cur_move_h.cur), move*/
-    top: -10px;
+    top: 0;
   }
 
   &-ref-dot-v {
@@ -438,18 +561,21 @@ export default {
     background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAMAAABFaP0WAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAZQTFRFf39/////F3PnHQAAAAJ0Uk5T/wDltzBKAAAAEElEQVR42mJgYGRgZAQIMAAADQAExkizYQAAAABJRU5ErkJggg==)
       repeat-y 1px top; /*./image/line_dot.png*/
     cursor: w-resize; /*url(./image/cur_move_v.cur), move*/
-    left: -10px;
+    left: 0;
   }
   &-content {
     position: absolute;
-    z-index: 997;
+    z-index: 2;
+    width: auto;
+    height: auto;
+    overflow: hidden;
   }
   &-content-mask{
     position: absolute;
     width: 100%;
     height: 100%;
     background: transparent;
-    z-index: 998;
+    z-index: 4;
   }
 }
 
